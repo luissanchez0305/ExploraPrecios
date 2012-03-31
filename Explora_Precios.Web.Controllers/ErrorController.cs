9ae02fbbeeac13a7fa0888923698f50507e3343f
Luis Ricardo Sanchez <luissanchez0305@hotmail.com>
2012-03-31 15:33:36 -0500

    error page creada

---
+++ Explora_Precios.Web.Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Explora_Precios.ApplicationServices;
+
+namespace Explora_Precios.Web.Controllers
+{
+	public class ErrorController : PrimaryController
+	{
+		public ActionResult Index()
+		{
+			var email = new EmailServices("info@exploraprecios.com", "Error", "Error YA! " + CurrentUser != null ? CurrentUser.username : "No user");
+			email.Send();
+			return View();
+		}
+	}
+}
